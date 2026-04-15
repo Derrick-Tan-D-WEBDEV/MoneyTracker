b8063e75b50b2bc2516a9b1998ce63987df7809a
Derrick-Tan-Aquametro <derrick.tan@aquametro-oil-marine.com.sg>
2026-04-16 00:09:30 +0800

    width fixed

--- src/components/csv-import-dialog.tsx
+++ src/components/csv-import-dialog.tsx
@@ -322,7 +322,7 @@ export function CSVImportDialog({ open, onOpenChange, accounts, onImported, defa
 
   return (
     <Dialog open={open} onOpenChange={handleClose}>
-      <DialogContent className="max-w-5xl max-h-[85vh] overflow-y-auto">
+      <DialogContent className="sm:max-w-5xl max-h-[85vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>
             {step === "upload" && "Import Transactions"}
